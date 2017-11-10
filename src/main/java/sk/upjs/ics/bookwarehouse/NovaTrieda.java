ccce9509ee723f6eb533c5355f3286408f03611c
Dell <Dell@Dell-PC>
2017-11-10 15:52:03 +0100

    This is the new GIT

---
+++ src/main/java/sk/upjs/ics/bookwarehouse/NovaTrieda.java
@@ -0,0 +1,11 @@
+package sk.upjs.ics.bookwarehouse;
+
+public class NovaTrieda {
+    public static void main(String[] args) {
+        System.out.println("ahojSvet");
+        System.out.println("nic");
+        System.out.println("Svet je krásny");
+        System.out.println("GIT je debilny nastroj");
+    }
+    
+}
